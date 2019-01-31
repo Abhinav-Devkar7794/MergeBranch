dec54016899e32e41e7e55fb155cc574351f083a
Abhinav-Devkar7794 <devkarabhinav@gmail.com>
2019-01-31 23:29:24 +0530

    listen and talk

--- CodeKing/src/com/axelor/code/Improvement.java
+++ CodeKing/src/com/axelor/code/Improvement.java
@@ -3,6 +3,7 @@ package com.axelor.code;
 public class Improvement {
 void communicationSkill() {
 	System.out.println("Tlak Politly and be Humble !!");
+	System.out.println("Listen More and Talk Less");
 }
 	
 }
