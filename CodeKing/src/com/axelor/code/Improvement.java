179d49e1011d002cd56f2e063e87844653ede259
Abhinav-Devkar7794 <devkarabhinav@gmail.com>
2019-01-31 23:31:12 +0530

    words

--- CodeKing/src/com/axelor/code/Improvement.java
+++ CodeKing/src/com/axelor/code/Improvement.java
@@ -2,6 +2,7 @@ package com.axelor.code;
 
 public class Improvement {
 void communicationSkill() {
+	System.out.println("Choose Always HGood Words In communication");
 	System.out.println("Tlak Politly and be Humble !!");
 }
 	
